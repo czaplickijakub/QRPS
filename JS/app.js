34922feb84f2f602daecab5c51bcdfb540fead35
Jakub <czaplickijakub1@gmail.com>
2022-01-11 08:32:37 -0500

    Functional Console Game

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,12 @@
+let computerScore = 0;
+let playerScore = 0;
+let flag = false;
+
 function giveResult(choice)
 {
-    let computerScore = 0;
-    let playerScore = 0;
+    if(flag) return;
+
     let computerChoice = compChoice();
-    //let computerChoice = compChoise;
 
     console.log('Player: ' + choice);
     console.log('Computer: ' + computerChoice);
@@ -79,11 +82,85 @@ function giveResult(choice)
             console.log('Player Wins!')
             playerScore++;
             break;
+    }
 
-        default: //temp to check computer 'mind'
+    switch(computerChoice + choice) //Computer win cases
+    {
+        //computer has rock
+        case 'rockscissor':
+            console.log('Rock crushes Scissors');
             console.log('Computer Wins!');
             computerScore++;
             break;
+        case 'rocklizard':
+            console.log('Rock crushes Lizard');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+
+        //computer has paper
+        case 'paperrock':
+            console.log('Paper covers Rock');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+        case 'paperspock':
+            console.log('Paper disproves Spock');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+
+        //computer has scissors
+        case 'scissorpaper':
+            console.log('Scissors cut Paper');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+        case 'scissorlizard':
+            console.log('Scissors decapitate Lizard');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+
+        //computer has lizard
+        case 'lizardpaper':
+            console.log('Lizard eats Paper');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+        case 'lizardspock':
+            console.log('Lizard poisons Spock');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+        
+        //computer has spock
+        case 'spockscissor':
+            console.log('Spock smashes Scissors');
+            console.log('Computer Wins!');
+            computerScore++;
+            break;
+        case 'spockrock':
+            console.log('Spock vaporizes Rock');
+            console.log('Computer Wins!')
+            computerScore++;
+            break;
+    }
+
+    console.log('Player Score is ' + playerScore);
+    console.log('Computer Score is ' + computerScore);
+    
+    if(computerScore == 5)
+    {
+        console.log('Computer Wins Game!');
+        flag = true;
+        return;
+    }
+    if(playerScore == 5)
+    {
+        console.log('Player Wins Game!');
+        flag = true;
+        return;
     }
 }
 
